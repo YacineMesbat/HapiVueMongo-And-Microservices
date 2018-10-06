d5cddb4d715f7297c50a06d54a3177d041bdf6ad
Yacine Mesbat <yacine.mesbat@gmail.com>
2018-10-06 18:49:22 +0200

    [Added] Crud on accountManager microservice

--- microservices/accountManager/www/app/Http/Controllers/UserController.php
+++ microservices/accountManager/www/app/Http/Controllers/UserController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
 use App\User;
 
 class UserController extends Controller
@@ -16,5 +18,50 @@ class UserController extends Controller
     public function delete($id)
     {
         User::findOrFail($id)->delete();
+    }
+    
+    public function create(Request $request)
+    {
+        $this->validate($request, [
+            'first_name'    => 'required|string|min:2',
+            'last_name'     => 'required|string|min:2',
+            'password'      => 'required|string|min:6',
+            'role'          => 'required|string|in:agent,detective,chef',
+            'email'         => 'required|email|unique:users'
+        ]);
+
+        return response(User::create([
+            'first_name'    => $request->first_name,
+            'last_name'     => $request->last_name,
+            'password'      => app('hash')->make($request->password),
+            'role'          => $request->role,
+            'email'         => $request->email
+        ]), 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'first_name'    => 'sometimes|string|min:2',
+            'last_name'     => 'sometimes|string|min:2',
+            'password'      => 'sometimes|string|min:6',
+            'role'          => 'sometimes|string|in:agent,detective,chef',
+            'email'         => 'sometimes|email|unique:users',
+            'confirmed'     => 'sometimes|boolean'
+        ]);
+
+        $request->password = app('hash')->make($request->password);
+
+        $user =  User::findOrFail($id);
+        $user->update($request->only([
+            'first_name',
+            'last_name',
+            'password',
+            'role',
+            'email',
+            'confirmed'
+        ]));
+
+        return ($user);
     }
 }
