7b7426a07a85eb2790849cc3d9fe6dd15f508d88
Yacine Mesbat <yacine.mesbat@gmail.com>
2018-10-10 14:30:33 +0200

    [Add] New Crime Controller

--- microservices/dataExporter/www/app/Crime.php
+++ microservices/dataExporter/www/app/Crime.php
@@ -11,6 +11,40 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
 class Crime extends Eloquent
 {
+
+    /**
+     * Collection
+     *
+     * @var string
+     */
     protected $collection = 'crimes';
+
+    /**
+     * Model primary Key
+     *
+     * @var string
+     */
     protected $primaryKey = '_id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'compnos', 'REPORTINGAREA', 'INCIDENT_TYPE_DESCRIPTION', 'REPTDISTRICT', 'FROMDATE',
+        'TODATE', 'STREETNAME', 'XSTREETNAME', 'WEAPONTYPE', 'BUILDINGTYPE', 'PLACEOFENTRY',
+        'PERPETRATORSNOS', 'SUSPECTTRANSPORTATION', 'VICTIMACTIVITY', 'UNUSUALACTIONS',
+        'WEATHER', 'NEIGHBORHOOD', 'LIGHTING', 'CLEARANCESTATUSDESC', 'MAIN_CRIMECODE',
+        'ROBBERY_TYPE', 'ROBBERY_ATTEMP', 'BURGLARY_TIME', 'DOMESTIC','WEAPON_TYPE',
+        'SHIFT', 'DAY_WEEK', 'UCRPART', 'X', 'Y', 'GREPORTING', 'GSECTOR', 'GBEAT',
+        'GDISTRICT', 'GDISTRICT_PRE2009', 'COMPUTEDCRIMECODE', 'COMPUTEDCRIMECODEDESC'
+    ];
+
+    /**
+     * Prevent Eloquent from adding fields created_at & updated_at
+     *
+     * @var string
+     */
+    public $timestamps = false;   
 }
--- microservices/dataExporter/www/app/Http/Controllers/CrimeController.php
+++ microservices/dataExporter/www/app/Http/Controllers/CrimeController.php
@@ -11,7 +11,7 @@ class CrimeController extends Controller
         if (isset($id))
             return response()->json(['Crime' => Crime::findOrFail($id)]);
         else
-            return response()->json(['Crimes' =>Crime::paginate(25)]);
+            return response()->json(['Crimes' => Crime::paginate(25)]);
     }
 
     public function delete($id)
@@ -22,55 +22,67 @@ class CrimeController extends Controller
     public function create(Request $request)
     {
         $this->validate($request, [
-            'compnos'                   => 'required|integer',
-            'REPORTINGAREA'             => 'sometimes|string|max:20',
-            'INCIDENT_TYPE_DESCRIPTION' => 'sometimes|string|max:200',
-            'REPTDISTRICT'              => 'sometimes|string|max:100',
-            'FROMDATE'                  => 'sometimes|date',
-            'TODATE'                    => 'sometimes|date',
-            'STREETNAME'                => 'sometimes|string|max:100',
-            'XSTREETNAME'               => 'sometimes|string|max:100',
-            'WEAPONTYPE'                => 'sometimes|string|max:100',
-            'BUILDINGTYPE'              => 'sometimes|string|max:100',
-            'PLACEOFENTRY'              => 'sometimes|string|max:100',
-            'PERPETRATORSNOS'           => 'sometimes|integer|between:-32768,32767',
-            'SUSPECTTRANSPORTATION'     => 'sometimes|string|max:100',
-            'VICTIMACTIVITY'            => 'sometimes|string|max:300',
-            'UNUSUALACTIONS'            => 'sometimes|string|max:300',
-            'WEATHER'                   => 'sometimes|string|max:100',
-            'NEIGHBORHOOD'              => 'sometimes|string|max:100',
-            'LIGHTING'                  => 'sometimes|string|max:100',
-            'CLEARANCESTATUSDESC'       => 'sometimes|string|max:100',
-            'MAIN_CRIMECODE'            => 'sometimes|string|max:15',
-            'ROBBERY_TYPE'              => 'sometimes|string|max:25',
-            'ROBBERY_ATTEMP'            => 'sometimes|string|max:10',
-            'BURGLARY_TIME'             => 'sometimes|string|max:10',
-            'DOMESTIC'                  => 'sometimes|string|max:10',
-            'WEAPON_TYPE'               => 'sometimes|string|max:100',
-            'SHIFT'                     => 'sometimes|string|max:50',
-            'DAY_WEEK'                  => 'sometimes|string|max:50',
-            'UCRPART'                   => 'sometimes|string|max:20',
-            'X'                         => 'sometimes|numeric',
-            'Y'                         => 'sometimes|numeric',
-            'GREPORTING'                => 'sometimes|string|max:50',
-            'GSECTOR'                   => 'sometimes|string|max:50',
-            'GBEAT'                     => 'sometimes|string|max:40',
-            'GDISTRICT'                 => 'sometimes|string|max:10',
-            'GDISTRICT_PRE2009'         => 'sometimes|string|max:10',
-            'COMPUTEDCRIMECODE'         => 'sometimes|string|max:20',
-            'COMPUTEDCRIMECODEDESC'     => 'sometimes|string|max:255'
+            'compnos'                   => 'required|integer|unique',
+            'naturecode'                => 'sometimes|string|max:100',
+            'shooting'                  => 'sometimes|string|max:100',
+            'reportingarea'             => 'sometimes|numeric',
+            'incident_type_description' => 'sometimes|string|max:200',
+            'reptdistrict'              => 'sometimes|string|max:100',
+            'fromdate'                  => 'sometimes|date',
+            'streetname'                => 'sometimes|string|max:100',
+            'xstreetname'               => 'sometimes|string|max:100',
+            'main_crimecode'            => 'sometimes|string|max:15',
+            'domestic'                  => 'sometimes|string|max:10',
+            'weapontype'                => 'sometimes|string|max:100',
+            'shift'                     => 'sometimes|string|max:50',
+            'day_week'                  => 'sometimes|string|max:50',
+            'month'                     => 'sometimes|integer|max:12',
+            'year'                      => 'sometimes|integer',
+            'ucrpart'                   => 'sometimes|string|max:20',
+            'x'                         => 'sometimes|numeric',
+            'y'                         => 'sometimes|numeric',
+            'location'                  => 'sometimes|string|max:100'
         ]);
 
-        return response(User::create([
-            $request->only([
-                'compnos', 'REPORTINGAREA', 'INCIDENT_TYPE_DESCRIPTION', 'REPTDISTRICT', 'FROMDATE',
-                'TODATE', 'STREETNAME', 'XSTREETNAME', 'WEAPONTYPE', 'BUILDINGTYPE', 'PLACEOFENTRY',
-                'PERPETRATORSNOS', 'SUSPECTTRANSPORTATION', 'VICTIMACTIVITY', 'UNUSUALACTIONS',
-                'WEATHER', 'NEIGHBORHOOD', 'LIGHTING', 'CLEARANCESTATUSDESC', 'MAIN_CRIMECODE',
-                'ROBBERY_TYPE', 'ROBBERY_ATTEMP', 'BURGLARY_TIME', 'DOMESTIC','WEAPON_TYPE',
-                'SHIFT', 'DAY_WEEK', 'UCRPART', 'X', 'Y', 'GREPORTING', 'GSECTOR', 'GBEAT',
-                'GDISTRICT', 'GDISTRICT_PRE2009', 'COMPUTEDCRIMECODE', 'COMPUTEDCRIMECODEDESC'
-            ])
-        ]), 201);
+        return response()->json(['Crime' => Crime::create($request->only([
+            "compnos", "naturecode", "incident_type_description", "main_crimecode", "reptdistrict",
+            "reportingarea", "fromdate", "weapontype", "shooting", "domestic", "shift", "year", 
+            "month", "day_week", "ucrpart", "x", "y", "streetname", "xstreetname", "location"
+        ]))], 201);
+    }
+
+    public function update(Request $request, $id)
+    {   
+        $this->validate($request, [
+            'naturecode'                => 'sometimes|string|max:100',
+            'shooting'                  => 'sometimes|string|max:100',
+            'reportingarea'             => 'sometimes|numeric',
+            'incident_type_description' => 'sometimes|string|max:200',
+            'reptdistrict'              => 'sometimes|string|max:100',
+            'fromdate'                  => 'sometimes|date',
+            'streetname'                => 'sometimes|string|max:100',
+            'xstreetname'               => 'sometimes|string|max:100',
+            'main_crimecode'            => 'sometimes|string|max:15',
+            'domestic'                  => 'sometimes|string|max:10',
+            'weapontype'                => 'sometimes|string|max:100',
+            'shift'                     => 'sometimes|string|max:50',
+            'day_week'                  => 'sometimes|string|max:50',
+            'month'                     => 'sometimes|integer|max:12',
+            'year'                      => 'sometimes|integer',
+            'ucrpart'                   => 'sometimes|string|max:20',
+            'x'                         => 'sometimes|numeric',
+            'y'                         => 'sometimes|numeric',
+            'location'                  => 'sometimes|string|max:100'
+        ]);
+
+        $crime = Crime::findOrFail($id);
+
+        $crime->update($request->only([
+            "naturecode", "incident_type_description", "main_crimecode", "reptdistrict",
+            "reportingarea", "fromdate", "weapontype", "shooting", "domestic", "shift", "year", 
+            "month", "day_week", "ucrpart", "x", "y", "streetname", "xstreetname", "location"
+        ]));
+
+        return response()->json(['Crime' => $crime], 201);
     }
 }
